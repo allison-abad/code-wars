7bcf3c8aa9719576e4315d583b31d94d53eb6110
Allison <39247861+allison-abad@users.noreply.github.com>
2018-07-03 22:42:20 -0400

    Create Is-num.js

---
+++ Is-num.js
@@ -0,0 +1,6 @@
+# Given a string s, write a method (function) that will return true if its a valid single integer or floating number or false if its not.
+
+
+function isDigit(s) {
+  return s==parseFloat(s);
+}
