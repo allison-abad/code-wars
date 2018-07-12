c6d8ba697783722aed4808da684fc51f1e606412
Allison <39247861+allison-abad@users.noreply.github.com>
2018-07-12 00:09:33 -0400

    Update sq.js

--- Square/sq.js
+++ Square/sq.js
@@ -1,7 +1,3 @@
- Given an integral number, determine if it's a square number:
- In mathematics, a square number or perfect square is an integer that is the square of an integer;
-in other words, it is the product of some integer with itself.
- The tests will always use some integral number, so don't worry about that in dynamic typed languages.
 
 
  var isSquare = function(n){
