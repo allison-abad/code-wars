6a980dd2f71cffc8d06beef05dd5d331596619cc
Allison <39247861+allison-abad@users.noreply.github.com>
2018-07-18 23:22:59 -0400

    Add files via upload

---
+++ list_filtering/list_filtering.js
@@ -0,0 +1,10 @@
+In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+
+function filter_list(l) {
+  // Return a new array with the strings filtered out
+  return l.reduce(function(a, b) {
+    if (typeof b === 'number') a.push(b);
+    return a;
+  }, []);
+}
